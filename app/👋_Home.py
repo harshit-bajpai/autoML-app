9ad53b6be88fb7e9d6be6512c07ba35f7c5c5436
Harshit Bajpai <harshit_bajpai@outlook.com>
2022-06-13 21:47:34 -0400

    added skeleton for basic app

---
+++ app/👋_Home.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(page_title="autoML", page_icon="👋")
+
+st.write("# AutoML Application👋")
+
+st.sidebar.success("Insert welcome text here")
+
+st.markdown(
+    """
+    ---
+    
+    ## Minimum Viable Product(MVP)
+
+    ### Supported Features:
+
+    - [ ] ETL
+    - [ ] Model Training
+    - [ ] Model Evaluation
+    - [ ] Export Configuration
+
+    """
+    )
